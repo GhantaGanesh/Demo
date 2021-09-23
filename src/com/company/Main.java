e2303d4f5e4d41bc21e436f1eca64725e6d54583
Ganesh <ghantasrisaiganesh@gmail.com>
2021-09-23 14:21:41 +0530

    made changes in code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	//System.out.println("Welcome");
-        System.out.println("Welcome to Demo");
+        System.out.println("Welcome to Demo1");
     }
 }
